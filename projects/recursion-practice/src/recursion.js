cbe8fe36c66db54614cf965bfe0971a2535eb30e
wendyver <ichigo.milk@me.com>
2024-04-16 10:37:47 -0500

    sum below

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -55,6 +55,13 @@ var isEven = function(n) {
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n > 0) {
+    return (n -1 + sumBelow(n -1));
+  } else if (n < 0) {
+    return (n +1) + sumBelow(n + 1)
+  }
 };
 
 // 6. Get the integers in range (x, y).
